d918b6530045f3b65d645c0e3c95ee73524de3c8
Michael Tsao <caoxiang@yeah.net>
2017-09-27 12:36:55 +0800

    部分修改表编码

--- console/migrations/m170923_083137_create_account_table.php
+++ console/migrations/m170923_083137_create_account_table.php
@@ -15,7 +15,7 @@ class m170923_083137_create_account_table extends Migration
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
         $this->createTable('account', [
